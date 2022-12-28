372a55f9a39546d7801c4557ed38e1f3c9a2a350
Ivonnemtz96 <ivonnemtz96@outlook.es>
2022-12-28 16:34:23 -0700

    Nuevos proyectos php, se quita footer extenso

---
+++ includes/scripts.php
@@ -0,0 +1,14 @@
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="icon fa fa-angle-up"></span></div>
+
+<script src="js/jquery.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/jquery.fancybox.js"></script>
+<script src="js/swiper.js"></script>
+<script src="js/owl.js"></script>
+<script src="js/appear.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/parallax.min.js"></script>
+<script src="js/custom-script.js"></script>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = 'Inicio';
+$inicio = 'active';
+include('includes/head.php');
+?>
+
+<body>
+    <div class="page-wrapper">
+        <?php
+        include('includes/header.php');
+        include('modules/index.php');
+        include('includes/footer.php');
+        include('includes/scripts.php');
+        ?>
+    </div>
+</body>
+
+</html>
